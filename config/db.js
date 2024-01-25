8b3bdc5f21ede25d26fd6f75d3ec408b9cec4030
nOSkr <118327643+nOSkr@users.noreply.github.com>
2024-01-25 20:32:33 +0800

    sync

--- config/db.js
+++ config/db.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
     useUnifiedTopology: true,
   });
 
