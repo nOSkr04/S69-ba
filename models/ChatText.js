760fd81fe50a4406d8b807a90a206df84b7dbaec
nOSkr <122243978+nOSkr04@users.noreply.github.com>
2024-03-21 18:04:53 +0800

    sync

---
+++ models/ChatText.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const ChatText = new mongoose.Schema(
+  {
+    role: String,
+    content: String,
+    createUser: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
+
+    updateUser: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+export default mongoose.model("Video", ChatText);
