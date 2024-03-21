a8f5b52f75701408f65c6db9d645066d2ab0a080
nOSkr <122243978+nOSkr04@users.noreply.github.com>
2024-03-21 18:13:08 +0800

    sync

--- routes/media.js
+++ routes/media.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { uploadPhoto, uploadVideo } from "../controller/media.js";
+import { uploadPhoto } from "../controller/media.js";
 
 const router = Router();
 
 router.route("/photo").post(uploadPhoto);
-router.route("/video").post(uploadVideo);
 
 export default router;
